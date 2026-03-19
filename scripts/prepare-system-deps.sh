630a3ca2d446df656b4621417169eeef677ba17b
Kevin Läufer <laeufer@cornell.edu>
2026-03-19 15:44:25 -0400

    run apt with sudo

--- scripts/prepare-system-deps.sh
+++ scripts/prepare-system-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install system dependencies
-apt-get update -q
-apt-get install -q -y build-essential python3-setuptools python3-dev python3-pip git wget ninja-build libevent-dev libjson-c-dev llvm-dev clang nodejs iverilog
+sudo apt-get update -q
+sudo apt-get install -q -y build-essential python3-setuptools python3-dev python3-pip git wget ninja-build libevent-dev libjson-c-dev llvm-dev clang nodejs iverilog
 pip3 install meson
