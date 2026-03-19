9134e56469c620082d6d5c5671c5fead296ed76c
Kevin Läufer <laeufer@cornell.edu>
2026-03-19 15:36:08 -0400

    install iverilog

--- scripts/prepare-system-deps.sh
+++ scripts/prepare-system-deps.sh
@@ -2,5 +2,5 @@
 
 # Install system dependencies
 apt-get update -q
-apt-get install -q -y build-essential python3-setuptools python3-dev python3-pip git wget ninja-build libevent-dev libjson-c-dev llvm-dev clang nodejs
+apt-get install -q -y build-essential python3-setuptools python3-dev python3-pip git wget ninja-build libevent-dev libjson-c-dev llvm-dev clang nodejs iverilog
 pip3 install meson
